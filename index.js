6d020c0a40befd2dd3b2331b4852b73000c54672
morrissh <morrissh@airproducts.com>
2017-01-17 08:59:39 -0800

    1. Hello World in Node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+﻿var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end('index.js on the server');
+}).listen(32456);
+console.log('Server running at port 32456');
