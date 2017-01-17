cc71618927dc56d1fc224da80431f5e1036c98e8
morrissh <morrissh@airproducts.com>
2017-01-17 09:28:21 -0800

    2. React's JSX Syntax

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-﻿var http = require('http');
+var http = require('http');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
 http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    res.end('index.js on the server');
+    res.end(
+        ReactDOMServer.renderToString(
+            React.createElement("html", null, 
+            React.createElement("head", null, 
+              React.createElement("title", null, "Hello World")
+            ), 
+            React.createElement("body", null, 
+              "index.jsx compiled into index.js by hand on the server"
+            )
+          )
+        )
+    );
 }).listen(32456);
 console.log('Server running at port 32456');
---
+++ index.jsx
@@ -0,0 +1,22 @@
+﻿var http = require('http');
+var React = require('react');
+var ReactDOMServer = require('react-dom/server');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(
+        ReactDOMServer.renderToString(
+            <html>
+            <head>
+              <title>Hello World</title>
+            </head>
+            <body>
+              index.jsx compiled into index.js by hand on the server
+            </body>
+          </html>
+        )
+    );
+}).listen(32456);
+console.log('Server running at port 32456');
