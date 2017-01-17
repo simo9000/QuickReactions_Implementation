dc7be134eea58c15237cc04269d03e6fd994a1bc
morrissh <morrissh@airproducts.com>
2017-01-17 11:14:13 -0800

    5. Gulp Restarts Node Automatically

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,18 @@
 ﻿var gulp = require('gulp')
-  , fs = require('fs')
-  , reactTools = require('react-tools')
-  , spawn = require('child_process').spawn;
+  , gulpReact = require('gulp-react')
+  , gulpNodemon = require('gulp-nodemon');
 
-var transform = function (srcFile, destFile, cb) {
-  console.log('Reading %s...', srcFile);
-  fs.readFile(srcFile, { encoding: 'utf8' }, function (readErr, data) {
-    if (readErr) {
-      cb(readErr);
-    }
-    else {
-      console.log('Writing %s', destFile)
-      fs.writeFile(destFile, reactTools.transform(data), function (writeErr) {
-        if (writeErr) {
-          cb(writeErr);
-        } else {
-          cb();
-        }
-      });
-    }
-  });
-}
-
-gulp.task('jsx', function (cb) {
-  fs.mkdir('./lib', function (err) {
-    transform('index.jsx', './lib/index.js', function (err) {
-      cb(err);
-    });
-  });
+gulp.task('jsx', function () {
+  return gulp.src('*.jsx')
+             .pipe(gulpReact())
+             .pipe(gulp.dest('lib'));
 });
 
-gulp.task('node', ['jsx'], function() {
-  spawn('node', ['./lib/index.js'], { stdio: 'inherit' });
+gulp.task('node', ['jsx'], function () {
+  gulpNodemon({
+    script: 'lib/index.js',
+    ext: 'js'
+  });
 });
 
 gulp.task('default', function () {
